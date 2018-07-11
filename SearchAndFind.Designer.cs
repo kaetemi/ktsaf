f991b11e46db2186907698876496739c821159dc
Jan Boon <kaetemi@no-break.space>
2018-07-11 19:04:27 +0800

    Search & Find (v1.4, 2011)

---
+++ SearchAndFind.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace ktsaf
+{
+	partial class SearchAndFind
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.keywordsText = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.locationEntry = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.browseButton = new System.Windows.Forms.Button();
+			this.searchButton = new System.Windows.Forms.Button();
+			this.progressBar = new System.Windows.Forms.ProgressBar();
+			this.label3 = new System.Windows.Forms.Label();
+			this.strategyDropDown = new System.Windows.Forms.ComboBox();
+			this.fileList = new System.Windows.Forms.ListView();
+			this.nameHeader = new System.Windows.Forms.ColumnHeader();
+			this.folderHeader = new System.Windows.Forms.ColumnHeader();
+			this.sizeHeader = new System.Windows.Forms.ColumnHeader();
+			this.typeHeader = new System.Windows.Forms.ColumnHeader();
+			this.modifiedHeader = new System.Windows.Forms.ColumnHeader();
+			this.fileRightClick = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.openSelectedDirectoriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.copyFullFileNamesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.sizeFilter = new System.Windows.Forms.ComboBox();
+			this.fileRightClick.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// keywordsText
+			// 
+			this.keywordsText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.keywordsText.Location = new System.Drawing.Point(77, 12);
+			this.keywordsText.Name = "keywordsText";
+			this.keywordsText.Size = new System.Drawing.Size(470, 20);
+			this.keywordsText.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 15);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(59, 13);
+			this.label1.TabIndex = 1000;
+			this.label1.Text = "Keywords: ";
+			// 
+			// locationEntry
+			// 
+			this.locationEntry.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.locationEntry.Location = new System.Drawing.Point(77, 38);
+			this.locationEntry.Name = "locationEntry";
+			this.locationEntry.Size = new System.Drawing.Size(314, 20);
+			this.locationEntry.TabIndex = 20;
+			this.locationEntry.Text = "C:\\";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 41);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(58, 13);
+			this.label2.TabIndex = 1000;
+			this.label2.Text = "Search in: ";
+			// 
+			// browseButton
+			// 
+			this.browseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.browseButton.Location = new System.Drawing.Point(397, 36);
+			this.browseButton.Name = "browseButton";
+			this.browseButton.Size = new System.Drawing.Size(75, 23);
+			this.browseButton.TabIndex = 30;
+			this.browseButton.Text = "Browse...";
+			this.browseButton.UseVisualStyleBackColor = true;
+			// 
+			// searchButton
+			// 
+			this.searchButton.Location = new System.Drawing.Point(12, 64);
+			this.searchButton.Name = "searchButton";
+			this.searchButton.Size = new System.Drawing.Size(75, 23);
+			this.searchButton.TabIndex = 50;
+			this.searchButton.Text = "Search";
+			this.searchButton.UseVisualStyleBackColor = true;
+			this.searchButton.Click += new System.EventHandler(this.searchButtonStart);
+			// 
+			// progressBar
+			// 
+			this.progressBar.Location = new System.Drawing.Point(93, 64);
+			this.progressBar.Name = "progressBar";
+			this.progressBar.Size = new System.Drawing.Size(100, 23);
+			this.progressBar.TabIndex = 1000;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(199, 69);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(371, 13);
+			this.label3.TabIndex = 1000;
+			this.label3.Text = "Enter one or more keywords seperated by spaces, and hit Search to find files.";
+			// 
+			// strategyDropDown
+			// 
+			this.strategyDropDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.strategyDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.strategyDropDown.FormattingEnabled = true;
+			this.strategyDropDown.Items.AddRange(new object[] {
+            "No subdirectories",
+            "Iterative (Recommended)",
+            "Depth-first"});
+			this.strategyDropDown.Location = new System.Drawing.Point(478, 37);
+			this.strategyDropDown.Name = "strategyDropDown";
+			this.strategyDropDown.Size = new System.Drawing.Size(150, 21);
+			this.strategyDropDown.TabIndex = 40;
+			// 
+			// fileList
+			// 
+			this.fileList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.fileList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameHeader,
+            this.folderHeader,
+            this.sizeHeader,
+            this.typeHeader,
+            this.modifiedHeader});
+			this.fileList.ContextMenuStrip = this.fileRightClick;
+			this.fileList.GridLines = true;
+			this.fileList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+			this.fileList.LabelWrap = false;
+			this.fileList.Location = new System.Drawing.Point(12, 93);
+			this.fileList.Name = "fileList";
+			this.fileList.ShowGroups = false;
+			this.fileList.Size = new System.Drawing.Size(616, 255);
+			this.fileList.TabIndex = 60;
+			this.fileList.UseCompatibleStateImageBehavior = false;
+			this.fileList.View = System.Windows.Forms.View.Details;
+			// 
+			// nameHeader
+			// 
+			this.nameHeader.Text = "Name";
+			this.nameHeader.Width = 120;
+			// 
+			// folderHeader
+			// 
+			this.folderHeader.Text = "Directory";
+			this.folderHeader.Width = 240;
+			// 
+			// sizeHeader
+			// 
+			this.sizeHeader.Text = "Size";
+			this.sizeHeader.Width = 120;
+			// 
+			// typeHeader
+			// 
+			this.typeHeader.Text = "Type";
+			// 
+			// modifiedHeader
+			// 
+			this.modifiedHeader.Text = "Modified";
+			this.modifiedHeader.Width = 120;
+			// 
+			// fileRightClick
+			// 
+			this.fileRightClick.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openSelectedDirectoriesToolStripMenuItem,
+            this.copyFullFileNamesToolStripMenuItem});
+			this.fileRightClick.Name = "fileRightClick";
+			this.fileRightClick.Size = new System.Drawing.Size(208, 48);
+			// 
+			// openSelectedDirectoriesToolStripMenuItem
+			// 
+			this.openSelectedDirectoriesToolStripMenuItem.Name = "openSelectedDirectoriesToolStripMenuItem";
+			this.openSelectedDirectoriesToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+			this.openSelectedDirectoriesToolStripMenuItem.Text = "Open selected directories";
+			this.openSelectedDirectoriesToolStripMenuItem.Click += new System.EventHandler(this.openSelectedDirectoriesToolStripMenuItem_Click);
+			// 
+			// copyFullFileNamesToolStripMenuItem
+			// 
+			this.copyFullFileNamesToolStripMenuItem.Name = "copyFullFileNamesToolStripMenuItem";
+			this.copyFullFileNamesToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+			this.copyFullFileNamesToolStripMenuItem.Text = "Copy file path(s)";
+			this.copyFullFileNamesToolStripMenuItem.Click += new System.EventHandler(this.copyFullFileNamesToolStripMenuItem_Click);
+			// 
+			// sizeFilter
+			// 
+			this.sizeFilter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.sizeFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.sizeFilter.FormattingEnabled = true;
+			this.sizeFilter.Items.AddRange(new object[] {
+            "Size",
+            "< 512 B",
+            "> 512 B",
+            "> 32 KiB",
+            "> 1 MiB",
+            "> 128 MiB",
+            "> 1 GiB"});
+			this.sizeFilter.Location = new System.Drawing.Point(553, 11);
+			this.sizeFilter.Name = "sizeFilter";
+			this.sizeFilter.Size = new System.Drawing.Size(75, 21);
+			this.sizeFilter.TabIndex = 10;
+			// 
+			// SearchAndFind
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(640, 360);
+			this.Controls.Add(this.sizeFilter);
+			this.Controls.Add(this.fileList);
+			this.Controls.Add(this.strategyDropDown);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.progressBar);
+			this.Controls.Add(this.searchButton);
+			this.Controls.Add(this.browseButton);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.locationEntry);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.keywordsText);
+			this.Name = "SearchAndFind";
+			this.Text = "Search & Find";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SearchAndFind_FormClosing);
+			this.fileRightClick.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox keywordsText;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox locationEntry;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button browseButton;
+		private System.Windows.Forms.Button searchButton;
+		private System.Windows.Forms.ProgressBar progressBar;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox strategyDropDown;
+		private System.Windows.Forms.ListView fileList;
+		private System.Windows.Forms.ColumnHeader nameHeader;
+		private System.Windows.Forms.ColumnHeader folderHeader;
+		private System.Windows.Forms.ColumnHeader sizeHeader;
+		private System.Windows.Forms.ColumnHeader typeHeader;
+		private System.Windows.Forms.ColumnHeader modifiedHeader;
+		private System.Windows.Forms.ContextMenuStrip fileRightClick;
+		private System.Windows.Forms.ToolStripMenuItem openSelectedDirectoriesToolStripMenuItem;
+		private System.Windows.Forms.ComboBox sizeFilter;
+		private System.Windows.Forms.ToolStripMenuItem copyFullFileNamesToolStripMenuItem;
+	}
+}
+
---
+++ SearchAndFind.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace ktsaf
+{
+	public partial class SearchAndFind : Form
+	{
+		public SearchAndFind()
+		{
+			InitializeComponent();
+			this.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+			strategyDropDown.SelectedIndex = 1;
+			sizeFilter.SelectedIndex = 0;
+		}
+
+		int sizeMinimum = 0;
+		int sizeMaximum = int.MaxValue;
+		string[] keywords = null;
+		DirectoryInfo searchDirectory;
+		Thread searchThread = null;
+
+		private void searchButtonStart(object sender, EventArgs e)
+		{
+			SearchStart();
+		}
+
+		private void searchButtonStop(object sender, EventArgs e)
+		{
+			SearchStop();
+		}
+
+		void SearchStart()
+		{
+			try
+			{
+				keywordsText.Enabled = false;
+				locationEntry.Enabled = false;
+				strategyDropDown.Enabled = false;
+				browseButton.Enabled = false;
+				searchButton.Text = "Stop";
+				searchButton.Click -= searchButtonStart;
+				searchButton.Click += searchButtonStop;
+				if (keywordsText.Text.Length <= 0)
+				{
+					if (sizeFilter.SelectedIndex != 0)
+					{
+						keywords = new string[0];
+						goto SkipKeywords;
+					}
+					goto FailKeywords;
+				}
+				keywords = keywordsText.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+				if (keywords.Length <= 0 && sizeFilter.SelectedIndex == 0) goto FailKeywords;
+				for (int i = 0; i < keywords.Length; ++i)
+					keywords[i] = keywords[i].ToLower();
+			SkipKeywords:
+				searchDirectory = new DirectoryInfo(locationEntry.Text);
+				if (!searchDirectory.Exists) goto FailDirectory;
+				switch (sizeFilter.SelectedIndex)
+				{
+					case 2:
+						sizeMinimum = 512;
+						break;
+					case 3:
+						sizeMinimum = 32 * 1024;
+						break;
+					case 4:
+						sizeMinimum = 1 * 1024 * 1024;
+						break;
+					case 5:
+						sizeMinimum = 128 * 1024 * 1024;
+						break;
+					case 6:
+						sizeMinimum = 1024 * 1024 * 1024;
+						break;
+					default:
+						sizeMinimum = 0;
+						break;
+				}
+				switch (sizeFilter.SelectedIndex)
+				{
+					case 1:
+						sizeMaximum = 512;
+						break;
+					default:
+						sizeMaximum = int.MaxValue;
+						break;
+				}
+				progressBar.Style = ProgressBarStyle.Marquee;
+				fileList.Items.Clear();
+
+				// ...
+				switch (strategyDropDown.SelectedIndex)
+				{
+					case 0:
+						searchThread = new Thread(new ThreadStart(NoSubSearch));
+						searchThread.Start();
+						break;
+					case 1:
+						searchThread = new Thread(new ThreadStart(IterativeSearch));
+						searchThread.Start();
+						break;
+					case 2:
+						searchThread = new Thread(new ThreadStart(DepthFirstSearch));
+						searchThread.Start();
+						break;
+					default:
+						goto FailStrategy; 
+				}
+				// ...
+
+				return;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+				SearchEnded();
+				return;
+			}
+		FailKeywords:
+			MessageBox.Show("You must enter at least one keyword.", "Enter keywords", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			SearchEnded();
+			return;
+		FailDirectory:
+			MessageBox.Show("The selected search directory does not exist. Please enter the correct path.", "Directory does not exist", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			SearchEnded();
+			return;
+		FailStrategy:
+			MessageBox.Show("The search strategy is invalid. Select a search strategy from the drop down menu.", "Invalid search strategy", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			SearchEnded();
+			return;
+		}
+
+		void SearchStop()
+		{
+			try
+			{
+				searchThread.Abort();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Abort failed", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+			}
+		}
+
+		void SearchEnded()
+		{
+			keywordsText.Enabled = true;
+			locationEntry.Enabled = true;
+			strategyDropDown.Enabled = true;
+			browseButton.Enabled = true;
+			searchButton.Text = "Start";
+			searchButton.Click += searchButtonStart;
+			searchButton.Click -= searchButtonStop;
+			progressBar.Style = ProgressBarStyle.Blocks;
+			searchThread = null;
+			keywords = null;
+		}
+
+		delegate void EmptyFunction();
+		delegate void ExceptionFunction(Exception ex);
+		void InvokeSearchEndedAbort()
+		{
+			if (InvokeRequired) { Invoke(new EmptyFunction(InvokeSearchEndedAbort)); return; }
+			if (!isClosing) MessageBox.Show("The search process has been aborted.", "Search aborted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			SearchEnded();
+		}
+
+		void InvokeSearchEndedException(Exception ex)
+		{
+			if (InvokeRequired) { Invoke(new ExceptionFunction(InvokeSearchEndedException), ex); return; }
+			if (!isClosing) MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+			SearchEnded();
+		}
+
+		void InvokeSearchEnded()
+		{
+			if (InvokeRequired) { Invoke(new EmptyFunction(InvokeSearchEnded)); return; }
+			SearchEnded();
+		}
+
+		void NoSubSearch()
+		{
+			try
+			{
+				foreach (DirectoryInfo subDir in searchDirectory.GetDirectories())
+				{
+					try
+					{
+						TestDirectory(subDir);
+					}
+					catch { }
+				}
+				foreach (FileInfo subFile in searchDirectory.GetFiles())
+				{
+					try
+					{
+						TestFile(subFile);
+					}
+					catch { }
+				}
+			}
+			catch (ThreadAbortException)
+			{
+				InvokeSearchEndedAbort();
+				return;
+			}
+			catch (Exception ex)
+			{
+				InvokeSearchEndedException(ex);
+				return;
+			}
+			InvokeSearchEnded();
+			return;
+		}
+
+		void IterativeSearch()
+		{
+			try
+			{
+				for (int searchDepth = 0; IterativeSearch(searchDirectory, 0, searchDepth) > 0; ++searchDepth) ;
+			}
+			catch (ThreadAbortException)
+			{
+				InvokeSearchEndedAbort();
+				return;
+			}
+			catch (Exception ex)
+			{
+				InvokeSearchEndedException(ex);
+				return;
+			}
+			InvokeSearchEnded();
+			return;
+		}
+
+		int IterativeSearch(DirectoryInfo dirInfo, int currentDepth, int searchDepth)
+		{
+			int subDirCount = 0;
+			if (currentDepth == searchDepth)
+			{
+				foreach (DirectoryInfo subDir in dirInfo.GetDirectories())
+				{
+					try
+					{
+						TestDirectory(subDir);
+						++subDirCount;
+					}
+					catch { }
+				}
+				foreach (FileInfo subFile in dirInfo.GetFiles())
+				{
+					try
+					{
+						TestFile(subFile);
+					}
+					catch { }
+				}
+			}
+			else
+			{
+				foreach (DirectoryInfo subDir in dirInfo.GetDirectories())
+				{
+					try
+					{
+						subDirCount += IterativeSearch(subDir, currentDepth + 1, searchDepth);
+					}
+					catch { }
+				}
+			}
+			return subDirCount;
+		}
+
+		void DepthFirstSearch()
+		{
+			try
+			{
+				DepthFirstSearch(searchDirectory);
+			}
+			catch (ThreadAbortException)
+			{
+				InvokeSearchEndedAbort();
+				return;
+			}
+			catch (Exception ex)
+			{
+				InvokeSearchEndedException(ex);
+				return;
+			}
+			InvokeSearchEnded();
+			return;
+		}
+
+		void DepthFirstSearch(DirectoryInfo dirInfo)
+		{
+			foreach (FileInfo subFile in dirInfo.GetFiles())
+			{
+				try
+				{
+					TestFile(subFile);
+				}
+				catch { }
+			}
+			foreach (DirectoryInfo subDir in dirInfo.GetDirectories())
+			{
+				try
+				{
+					TestDirectory(subDir);
+				}
+				catch { }
+				try
+				{
+					DepthFirstSearch(subDir);
+				}
+				catch { }
+			}
+		}
+
+		void TestFile(FileInfo fileInfo)
+		{
+			if (fileInfo.Length < sizeMinimum || fileInfo.Length > sizeMaximum)
+				return;
+			string name = fileInfo.Name.ToLower();
+			foreach (string keyword in keywords)
+			{
+				if (!name.Contains(keyword))
+				{
+					return;
+				}
+			}
+			InvokeFoundFile(fileInfo);
+		}
+
+		void TestDirectory(DirectoryInfo dirInfo)
+		{
+			if (keywords.Length == 0) return;
+			string name = dirInfo.Name.ToLower();
+			foreach (string keyword in keywords)
+			{
+				if (!name.Contains(keyword))
+				{
+					return;
+				}
+			}
+			InvokeFoundDir(dirInfo);
+		}
+
+		delegate void FileInfoFunction(FileInfo fileInfo);
+		void InvokeFoundFile(FileInfo fileInfo)
+		{
+			if (InvokeRequired) { Invoke(new FileInfoFunction(InvokeFoundFile), fileInfo); return; }
+			fileList.Items.Add(new ListViewItem(new string[] { fileInfo.Name, fileInfo.Directory.FullName, fileInfo.Length.ToString(), fileInfo.Extension, fileInfo.LastWriteTime.ToString() }));
+		}
+
+		delegate void DirectoryInfoFunction(DirectoryInfo dirInfo);
+		void InvokeFoundDir(DirectoryInfo dirInfo)
+		{
+			if (InvokeRequired) { Invoke(new DirectoryInfoFunction(InvokeFoundDir), dirInfo); return; }
+			fileList.Items.Add(new ListViewItem(new string[] { dirInfo.Name, dirInfo.FullName, "", "<DIR>", dirInfo.LastWriteTime.ToString() }));
+		}
+
+
+		private void openSelectedDirectoriesToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			foreach (ListViewItem lvi in fileList.SelectedItems)
+			{
+				System.Diagnostics.Process.Start(lvi.SubItems[1].Text);
+			}
+		}
+
+
+		private void copyFullFileNamesToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			StringBuilder builder = new StringBuilder();
+			foreach (ListViewItem lvi in fileList.SelectedItems)
+			{
+				builder.Append(lvi.SubItems[1].Text);
+				builder.Append("\\");
+				builder.Append(lvi.SubItems[0].Text);
+				builder.Append("\r\n");
+			}
+			builder.Length -= 2;
+			Clipboard.SetText(builder.ToString());
+		}
+
+
+		bool isClosing = false;
+		private void SearchAndFind_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			isClosing = true;
+			if (searchThread != null)
+			{
+				if (e.CloseReason == CloseReason.UserClosing)
+				{
+					if (MessageBox.Show("A search is still in progress.\r\nAre you sure you want to close?", "Search in progress", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+					{
+						isClosing = false;
+						e.Cancel = true;
+						return;
+					}
+				}
+				searchThread.Abort();
+			}
+		}
+	}
+}
